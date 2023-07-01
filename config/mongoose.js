926ff677e549baee443cfbd0849e6108d2d7112c
abhishek270101 <109814206+abhishek270101@users.noreply.github.com>
2023-07-01 15:44:33 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const mongoDB = "mongodb+srv://new_CSV:new_CSV@cluster0.gbjrkub.mongodb.net/my_database?retryWrites=true&w=majority"; 
-//const mongoDB = "mongodb://127.0.0.1/my_database"; 
+const mongoDB = "mongodb+srv://new_CSV:new_CSV@cluster0.gbjrkub.mongodb.net/my_database?retryWrites=true&w=majority";  
 main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
