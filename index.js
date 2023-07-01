8d37201202bdfe3ecf361bd8037b0adef9963a10
abhishek270101 <109814206+abhishek270101@users.noreply.github.com>
2023-07-01 15:09:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// // const { urlencoded } = require('express');
 const express = require('express');
 const path = require('path');
 const port = 8000;
@@ -13,32 +12,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('assets'));
 
-// var contactList = [
-//     {
-//         name: "Pooja",
-//         phone: "88993894828"
-//     },
-//     {
-//         name: "pihu",
-//         phone: "8899601557"
-//     },
-//     {
-//         name: "Akshay",
-//         phone: "7216742270"
-//     },
-//     {   
-//         name: "Mahima",
-//         phone: "91944662634"
-//     }
-// ]
-
 app.get('/', function(req, res){
     Contact.find({}, function(err, contacts){
         if(err){
             console.log('error', err); 
             return; 
         }
-        //console.log(contacts);
+        
         return res.render('home', {
             title: "Contact List",
             contact_list: contacts
@@ -48,16 +28,8 @@ app.get('/', function(req, res){
     
 });
 
-
-
-// app.get('/practice', function(req, res){
-//     return res.render('practice', {
-//         title: "Playing with EJS",
-//     });
-// });
-
 app.post('/create-contact', async function(req,res){
-    // contactList.push(req.body)
+   
     let newContact  =  Contact.create({
         name: req.body.name,
         phone: req.body.phone
@@ -65,14 +37,13 @@ app.post('/create-contact', async function(req,res){
     if(newContact){
         return res.redirect('back');
     }
-    // return res.redirect('back');
+   
 });
 
 app.get('/delete-contact/', function(req, res){
-    // console.log(req.query);
-    //get the if from query in url
+
     let id = req.query.id;
-    //find the contact in database using id abd delete
+    
     Contact.findByIdAndDelete(id, function(err){
         if(err){
             console.log('error', err); 
